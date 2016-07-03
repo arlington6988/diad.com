18a57ce2b3d9566173e5b6912cf1c9e4f35ce667
arlington6988 <arlington6988@yahoo.com>
2016-07-03 19:48:34 -0400

    adding new style to music background page

--- diad/config/initializers/assets.rb
+++ diad/config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( cassette-tape-ui.css cassette-tape-ui-blur.css cassette-tape-ui-blur-nojs.css diadmain.css diadmusic.css soundmanager2.js cassette-tape-ui.js )
+Rails.application.config.assets.precompile += %w( cassette-tape-ui.css cassette-tape-ui-blur.css cassette-tape-ui-blur-nojs.css diadmain.css diadsimplemusic.css diadmusic.css soundmanager2.js cassette-tape-ui.js )
